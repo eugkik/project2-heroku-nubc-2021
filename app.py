7ce83b50b08b65d172d750490fe40c2efe4f0e23
Eugene Kikuchi <eki326@ads.northwestern.edu>
2021-10-28 18:51:49 -0500

    changed to state code

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from sqlalchemy.engine import create_engine
 
 app = Flask(__name__)
 
-#pw = 'kyler20'
 pw = 'TZFLAhpQ6P1tPJzfXg3MoUnfUIOcHcd2'
 
 #DATABASE_URI = f'postgresql+psycopg2://postgres:{pw}@localhost:5432/Project2_Dev'
@@ -54,15 +53,15 @@ def fips(geo_fips):
     #return (fip)
     return jsonify(result_list)
 
-@app.route("/api/total2/<state>")
-def total2(state):
-    result = engine.execute(f"select \"2019\" from us_expenditures where GeoName = '{state}' AND Description = 'Personal consumption expenditures'")
+@app.route("/api/total2/<code>")
+def total2(code):
+    result = engine.execute(f"select dollars from us_expenditure where code = '{code}' AND description = 'Personal consumption expenditures' AND year = 2019")
     r = result.fetchone()
     return str(round(r[0]))
 
-@app.route("/api/total1/<state>")
-def total1(state):
-    result = engine.execute(f"select \"2019\" from us_expenditures where GeoName = '{state}' AND Description = 'Personal consumption expenditures'")
+@app.route("/api/total1/<code>")
+def total1(code):
+    result = engine.execute(f"select dollars from us_expenditure where code = '{code}' AND description = 'Personal consumption expenditures' AND year = 2019")
     rows = result.fetchall()
     result_list = []
     for r in rows:
