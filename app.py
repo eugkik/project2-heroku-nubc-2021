78b721e21b03a2edca847e3c7733785c100d2832
Eugene Kikuchi <eki326@ads.northwestern.edu>
2021-10-29 16:12:41 -0500

    added comments

--- app.py
+++ app.py
@@ -13,9 +13,10 @@ DATABASE_URI = f'postgresql+psycopg2://kwwhumda:{pw}@fanny.db.elephantsql.com:54
 
 engine = create_engine(DATABASE_URI)
 
+# Return entire dataframe
 @app.route("/api")
 def all_data():
-    result = engine.execute("select * from us_expenditure")
+    result = engine.execute("select * from us_spend_df")
     rows = result.fetchall()
     result_list = []
     for r in rows:
@@ -23,9 +24,10 @@ def all_data():
 
     return jsonify(result_list)
 
+# Return everything given state name
 @app.route("/api/<state>")
 def state(state):
-    result = engine.execute(f"select * from us_expenditure where GeoName = '{state}'")
+    result = engine.execute(f"select * from us_spend_df where GeoName = '{state}'")
     rows = result.fetchall()
     result_list = []
     for r in rows:
@@ -33,9 +35,10 @@ def state(state):
 
     return jsonify(result_list)
 
+# Return state name, description,year given state name, year
 @app.route("/api/<state>/<year>")
 def year(state, year):
-    result = engine.execute(f"select GeoName, Description, `{year}` from us_expenditures where GeoName = '{state}'")
+    result = engine.execute(f"select geoname, description, `{year}` from us_spend_df where geoname = '{state}'")
     rows = result.fetchall()
     result_list = []
     for r in rows:
@@ -43,32 +46,37 @@ def year(state, year):
 
     return jsonify(result_list)
 
+# Return everything given GeoFIPS
 @app.route("/api/fips/<geo_fips>")
 def fips(geo_fips):
-    result = engine.execute(f"select * from us_expenditure where geofips = '{geo_fips}'")
+    result = engine.execute(f"select * from us_spend_df where geofips = '{geo_fips}'")
     rows = result.fetchall()
     result_list = []
     for r in rows:
        result_list.append(dict(r))
-    #fips = f'\"{fips}\"'
-    #return (fip)
+    
     return jsonify(result_list)
 
+# Schaefer
+# Return dollars for Personal Consumption for 2019 given state abbr
 @app.route("/api/total2/<code>")
 def total2(code):
-    result = engine.execute(f"select dollars from us_expenditure where code = '{code}' AND description = 'Personal consumption expenditures' AND year = 2019")
+    result = engine.execute(f"select dollars from us_spend_df where code = '{code}' AND description = 'Personal consumption expenditures' AND year = 2019")
     r = result.fetchone()
     return str(round(r[0]))
 
+# Schaefer
+# Same as above
 @app.route("/api/total1/<code>")
 def total1(code):
-    result = engine.execute(f"select dollars from us_expenditure where code = '{code}' AND description = 'Personal consumption expenditures' AND year = 2019")
+    result = engine.execute(f"select dollars from us_spend_df where code = '{code}' AND description = 'Personal consumption expenditures' AND year = 2019")
     rows = result.fetchall()
     result_list = []
     for r in rows:
         result_list.append(dict(r))
     return jsonify(result_list)
 
+# Return state name, description, year, and dollars
 @app.route("/api/all_years")
 def total_years():
     result = engine.execute(f"select geoname, description, year, dollars FROM us_spend_df")
@@ -78,6 +86,8 @@ def total_years():
         result_list.append(dict(r))
     return jsonify(result_list)
 
+# Michael
+# Return state name, description, year, dollars given state abbr and description
 @app.route("/api/all_years/<code>/<desc>")
 def total_years_state(code, desc):
     result = engine.execute(f"select geoname, description, year, dollars FROM us_spend_df WHERE code = '{code}' AND description = '{desc}'")
@@ -87,6 +97,8 @@ def total_years_state(code, desc):
         result_list.append(dict(r))
     return jsonify(result_list)
 
+# Michael
+# Return state name, description, year, dollars given state abbr, description, and year
 @app.route("/api/by_year/<code>/<desc>/<year>")
 def total_by_years_state(code, desc, year):
     result = engine.execute(f"select geoname, description, year, dollars FROM us_spend_df WHERE code = '{code}' AND description = '{desc}' AND year = {year}")
@@ -96,6 +108,8 @@ def total_by_years_state(code, desc, year):
         result_list.append(dict(r))
     return jsonify(result_list)
 
+# Schaefer
+# Return state abbr, dollars for Personal Consumption for given year
 @app.route("/api/exp_by_year/<year>")
 def exp_by_year(year):
     result = engine.execute(f"select code, dollars from us_spend_df where description = 'Personal consumption expenditures' AND year = '{year}'")
