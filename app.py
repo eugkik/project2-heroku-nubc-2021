2a1296695264b67d9cbe36a75e76b27dd2a9a8db
Eugene Kikuchi <eki326@ads.northwestern.edu>
2021-10-28 20:48:21 -0500

    added expbyyear

--- app.py
+++ app.py
@@ -78,5 +78,14 @@ def total_years():
         result_list.append(dict(r))
     return jsonify(result_list)
 
+@app.route("/api/exp_by_year/<year>")
+def exp_by_year(year):
+    result = engine.execute(f"select geoname, dollars from us_spend_df where description = 'Personal consumption expenditures' AND year = '{year}'")
+    rows = result.fetchall()
+    result_list = []
+    for r in rows:
+        result_list.append(dict(r))
+    return jsonify(result_list)
+
 if __name__ == '__main__':
     app.run(debug=True)
